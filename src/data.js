40eea2308cfd0370ddc6ea02a5fcbf7703018909
parappa20 <jamesaaronlindsey182@gmail.com>
2022-06-15 16:03:56 -0500

    set up initial structure and layout

---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const projects = [
+    {
+        title: "PlayTrack",
+        subtitle: "Mongo, Apollo, GraphQL, React",
+        description: "PlayTrack is a social media site based around casual gaming, where you can track your completed games and share updates with your friends.",
+        image: './',
+        link: "playtrackgames.herokuapp.com"
+    },
+    {
+        title: "SouSmart",
+        subtitle: "Mongo, Apollo, GraphQL, React",
+        description: "SouSmart is a website that will give you recipes you can make without having to leave home. Input the ingredients you have, refine your search, and see what you can whip up.",
+        image: './',
+        link: 'sousmart.herokuapp.com'
+    }
+]
