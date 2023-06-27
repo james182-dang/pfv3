c1812d6a0c7818316878b840d5b823042019ca16
parappa20 <jamesaaronlindsey182@gmail.com>
2023-06-27 11:07:36 -0500

    update skills, about section

---
+++ src/components/Roll.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Roll from 'react-reveal/Roll';
+
+class RollExample extends React.Component {
+    render() {
+        return (
+            <div>
+                <Roll left>
+                </Roll>
+            </div>
+        )
+    }
+}
+
+export default RollExample;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,6 @@
 import { BadgeCheckIcon, ChipIcon } from '@heroicons/react/solid';
 import { skills } from '../data';
+import Roll from 'react-reveal';
 
 function Skills() {
     return (
@@ -16,6 +17,7 @@ function Skills() {
                 </div>
                 <div className='flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2'>
                     {skills.map((skill) => (
+                        <Roll left duration={2000}>
                         <div key={skill} className='p-2 sm:w-1/2 w-full'>
                             <div className='bg-gray-800 rounded flex p-4 h-full items-center'>
                                 <BadgeCheckIcon className='text-green-400 w-6 h-6 flex-shrink-0 mr-4' />
@@ -24,6 +26,7 @@ function Skills() {
                                 </span>
                             </div>
                         </div>
+                        </Roll>
                     ))}
                 </div>
             </div>
